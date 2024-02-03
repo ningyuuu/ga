2f25857b20d753d71d313c9611fbb8bb878aa866
ningyu <ningyu@estl.sg>
2024-02-03 19:50:06 +0800

    done with proj 2

--- proj/2/findX.py
+++ proj/2/findX.py
@@ -27,6 +27,37 @@ def findXinA(x, findX):
     #TODO Your Code Begins Here, DO NOT MODIFY ANY CODE ABOVE THIS LINE
 
     theIndex = None # replace None with the index of x
+    lowerIdx = 1
+    upperIdx = 1
+
+    upperVal = None
+    foundStart = False
+
+    while not foundStart and theIndex is None:
+        upperVal = findX.lookup(upperIdx)
+
+        if upperVal == x:
+            theIndex = upperIdx
+        elif upperVal is not None and upperVal < x:
+            lowerIdx = upperIdx
+            upperIdx *= 2
+        else:
+            foundStart = True
+
+    while theIndex is None:
+        lookupIdx = (upperIdx + lowerIdx) // 2
+
+        if (upperIdx - 1) == lowerIdx and lookupIdx == lowerIdx:
+            break
+
+        lookupVal = findX.lookup(lookupIdx)
+
+        if lookupVal == x:
+            theIndex = lookupIdx
+        elif lookupVal is None or lookupVal > x:
+            upperIdx = lookupIdx
+        else:
+            lowerIdx = lookupIdx
 
     #TODOne Your code Ends here, DO NOT MOIDFY ANYTHING BELOW THIS LINE
 
